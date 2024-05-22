deb681118170e71f0556a5c8f9f83e9a47563b0b
Luiz Carlos <argentinaluiz@gmail.com>
2024-05-22 03:32:02 -0300

    Add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,25 +5,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CacheModule } from '@nestjs/cache-manager';
 import type { RedisClientOptions } from 'redis';
 import { redisStore } from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nest',
-      entities: [],
-      synchronize: true,
-    }),
-    CacheModule.register<RedisClientOptions>({
-      store: redisStore as any,
-
-      // Store-specific configuration:
-      url: 'redis://localhost:6379/0',
+    ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: process.env.TYPEORM_CONNECTION as any,
+        host: process.env.TYPEORM_HOST,
+        port: parseInt(process.env.TYPEORM_PORT),
+        username: process.env.TYPEORM_USERNAME,
+        password: process.env.TYPEORM_PASSWORD,
+        database: process.env.TYPEORM_DATABASE,
+        entities: [],
+        synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      }),
+    }),
+    CacheModule.registerAsync<RedisClientOptions>({
+      useFactory: () => ({
+        store: redisStore as any,
+        url: process.env.REDIS_DSN,
+      }),
     }),
   ],
   controllers: [AppController],
