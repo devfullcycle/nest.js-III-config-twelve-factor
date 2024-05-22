07ac1d5462986543c84980011430ea89bcaa38c4
Luiz Carlos <argentinaluiz@gmail.com>
2024-05-22 03:42:07 -0300

    feat: Add joi package for input validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,30 +5,54 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CacheModule } from '@nestjs/cache-manager';
 import type { RedisClientOptions } from 'redis';
 import { redisStore } from 'cache-manager-redis-store';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+
+export type Config = {
+  TYPEORM_CONNECTION: string;
+  TYPEORM_HOST: string;
+  TYPEORM_PORT: number;
+  TYPEORM_USERNAME: string;
+  TYPEORM_PASSWORD: string;
+  TYPEORM_DATABASE: string;
+  TYPEORM_SYNCHRONIZE: boolean;
+  REDIS_DSN: string;
+};
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        TYPEORM_CONNECTION: Joi.string().valid('mysql').required(),
+        TYPEORM_HOST: Joi.string().required(),
+        TYPEORM_PORT: Joi.number().required(),
+        TYPEORM_USERNAME: Joi.string().required(),
+        TYPEORM_PASSWORD: Joi.string().required(),
+        TYPEORM_DATABASE: Joi.string().required(),
+        TYPEORM_SYNCHRONIZE: Joi.boolean().required(),
+        REDIS_DSN: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: process.env.TYPEORM_CONNECTION as any,
-        host: process.env.TYPEORM_HOST,
-        port: parseInt(process.env.TYPEORM_PORT),
-        username: process.env.TYPEORM_USERNAME,
-        password: process.env.TYPEORM_PASSWORD,
-        database: process.env.TYPEORM_DATABASE,
+      useFactory: (configService: ConfigService<Config>) => ({
+        type: configService.get('TYPEORM_CONNECTION') as any,
+        host: configService.get('TYPEORM_HOST'),
+        port: configService.get('TYPEORM_PORT') as number,
+        username: configService.get('TYPEORM_USERNAME') as string,
+        password: configService.get('TYPEORM_PASSWORD') as string,
+        database: configService.get('TYPEORM_DATABASE') as string,
         entities: [],
-        synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+        synchronize: configService.get('TYPEORM_SYNCHRONIZE') as boolean,
       }),
+      inject: [ConfigService],
     }),
     CacheModule.registerAsync<RedisClientOptions>({
-      useFactory: () => ({
+      useFactory: (configService: ConfigService<Config>) => ({
         store: redisStore as any,
-        url: process.env.REDIS_DSN,
+        url: configService.get('REDIS_DSN'),
       }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
